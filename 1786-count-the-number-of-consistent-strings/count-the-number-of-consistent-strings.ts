a4be1267731d0e0003bbb4c6d6b0944cc6ccd6e0
Bakdaulet06 <baikozhabakdaulet7@gmail.com>
2026-01-28 22:33:21 +0500

    Time: 72 ms (65.00%) | Memory: 64.8 MB (50.00%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.ts
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.ts
@@ -1,10 +1,14 @@
 function countConsistentStrings(allowed: string, words: string[]): number {
     let count: number = 0
     for(let i = 0; i<words.length; i++){
+        let consistent = true
         for(let j = 0; j<words[i].length; j++){
-            if(!allowed.includes(words[i][j])) break
-            if(j === words[i].length-1) count++
+            if(!allowed.includes(words[i][j])){
+                consistent = false
+                break
+            }
         }
+        if(consistent) count++
     }
     return count
 };
